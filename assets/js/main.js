9b9f267dfdf438ee88ec0aa2a93c862965b41bf3
Tony Ames <tonyames.pro@gmail.com>
2019-11-21 13:17:47 +0000

    correction des textes

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,12 +4,30 @@ $(function () {
     $(document).scroll(function () {
         var $nav = $(".fixed-top");
         $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-        if ($nav.offset().top > $nav.height()) {
+        if ($nav.offset().top > 10) {
             
             $("#logo").attr("src", "./assets/img/Logo-dark.png")
         } else {
 
             $("#logo").attr("src", "./assets/img/Logo.png")
+        }
+    });
+});
+
+$(function () {
+    /**
+    * Smooth scrolling to page anchor on click
+    **/
+    $("a[href*='#']:not([href='#'])").click(function () {
+        if (
+            location.hostname == this.hostname
+            && this.pathname.replace(/^\//, "") == location.pathname.replace(/^\//, "")
+        ) {
+            var anchor = $(this.hash);
+            anchor = anchor.length ? anchor : $("[name=" + this.hash.slice(1) + "]");
+            if (anchor.length) {
+                $("html, body").animate({ scrollTop: anchor.offset().top }, 1500);
+            }
         }
     });
 });
